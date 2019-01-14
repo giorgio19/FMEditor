c929b9226850f81df40721992f31135cb678f873
giorgio19 <giorgio.catania@pepperdine.edu>
2019-01-13 18:25:15 -0800

    css updates following notetron model

--- main.js
+++ main.js
@@ -1,44 +1,51 @@
-const{ remote, ipcRenderer} = require('electron')
-const{ app, BrowserWindow} = require('electron')
+'use strict';
+
+const electron = require('electron');
+const{ remote, ipcRenderer} = require('electron');
+const{ app, BrowserWindow} = require('electron');
 
 
-function createWindow (){
-  //creates the browser createWindow
-  win = new BrowserWindow({width: 800, height: 600})
+var mainWindow = null;
 
-  //load the index.html
-  win.loadFile('index.html')
-
-  //open DevTools
-  win.webContents.openDevTools()
-
-  //emit when the window is closed
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-   // in an array if your app supports multi windows, this is the time
-   // when you should delete the corresponding element.
-   win = null
-  })
-}
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready',createWindow)
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({
+    'width': 800,
+    'height': 600,
+    'minWidth': 360,
+    'minHeight': 300,
+    'frame': false
+  });
+  //load the index.html
+  mainWindow.loadFile('index.html');
+  //open DevTools
+  mainWindow.webContents.openDevTools();
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+  //emit when the window is closed
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+   // in an array if your app supports multi windows, this is the time
+   // when you should delete the corresponding element.
+   mainWindow = null;
+ });
+});
+
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
+  if (mainWindow === null) {
+    createWindow();
   }
-})
+});
