a62177b289691de3d750e60c4612a256691fed42
giorgio19 <giorgio.catania@pepperdine.edu>
2019-01-23 16:17:27 -0800

    implemented keybindings using Prof Cupp's method

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const remote = require('electron').remote;
 const dialog = remote.require('electron').dialog;
 const SlickCompiler = require('./Antlr/SlickCompiler').SlickCompiler
-
+const Quill = require('quill');
+var Keyboard = Quill.import('modules/keyboard');
 
 const padding = '     ';
 const spacing = 5;
@@ -62,54 +63,6 @@ $(".panel-top").resizable({
   resizeWidth: false
 });
 
-function bindKey(quill, text, prefix, key, shift, replace, hint, pair, quant) {
-  // console.log('keySeq type = ' + typeof keySeq);
-  // const key: string = (typeof keySeq === 'number' ? keySeq : keySeq.substr(keySeq.length - 1, 1));
-  // const prefix: string = (typeof keySeq === 'number' ? '' : (keySeq.length > 1 ? keySeq.substr(0, keySeq.length - 1) : ''));
-  let bindObj;
-  if (shift) {
-    bindObj = {key: key, shiftKey: true};
-  } else {
-    bindObj = {key: key};
-  }
-  const anchor = (hint ? '\\s{5}' : '\\S*');
-  const bindOptions = {
-    collapsed: true,
-    prefix: new RegExp(anchor + prefix + '$'),
-    // offset: -10
-  };
-  if (hint) {
-    // bindOptions.offset = 5 + prefix.length;
-  }
-  quill.keyboard.addBinding(bindObj, bindOptions,
-  (range, context) => {
-    quill.format('bold', false);
-    quill.format('italic', false);
-    let preLength = prefix.length;
-    if (prefix.startsWith('\\')) {
-        preLength--;
-    }
-    const off = (hint ? this.spacing + preLength : preLength);
-    if (replace) {
-      quill.deleteText(range.index - off, off);
-    }
-    quill.insertText(range.index - (replace ? off : 0), text);
-    quill.setSelection(range.index + (hint ? + text.length - 7 - preLength : preLength + text.length - (pair ? 1 : quant ? 11 : 2)));
-  });
-}
-
-function bindHint(quill, text, prefix, key, shift, replace) {
-  this.bindKey(quill, text, prefix, key, shift, replace, true);
-}
-
-function bindPair(quill, text, prefix, key, shift, replace) {
-  this.bindKey(quill, text, prefix, key, shift, replace, false, true);
-}
-
-function bindQuant(quill, text, prefix, key, shift, replace) {
-  this.bindKey(quill, text, prefix, key, shift, replace, false, false, true);
-}
-
 var bindings = {
   padEnter:{
     key: 13,
@@ -117,122 +70,105 @@ var bindings = {
     handler: function(range,context){
       this.quill.insertText(range.index, '\n     ')
     }
-  },
-  implies:{
-    key: 'm',
-    empty:false,
-    prefix: /;i$/,
-    handler: function(range,context){
-        this.quill.deleteText(range.index - 2, 2);
-        if(context.offset == 2){
-          this.quill.insertText(range.index - 2, impliesUnicode + hintUnicode);
-          this.quill.setSelection(range.index + 6);
-        } else {
-          this.quill.insertText(range.index - 2, ' ' + impliesUnicode + ' ');
-          this.quill.setSelection(range.index + 1);
-        }
-      }
-    },
-  follows:{
-      key: 'f',
-      empty:false,
-      prefix: /;f$/,
-      handler: function(range,context){
-          this.quill.deleteText(range.index - 2, 2);
-          if(context.offset == 2){
-            this.quill.insertText(range.index - 2, followsFromUnicode + hintUnicode);
-            this.quill.setSelection(range.index + 6);
-          } else {
-            this.quill.insertText(range.index - 2, ' ' + followsFromUnicode + ' ');
-            this.quill.setSelection(range.index + 1);
-          }
-        }
-      },
-  equals:{
-    key: 'q',
-    empty:false,
-    prefix: /;e$/,
-    handler: function(range,context){
-        this.quill.deleteText(range.index - 2, 2);
-        if(context.offset == 2){
-          this.quill.insertText(range.index - 2, equalsUnicode + "  " + hintUnicode);
-          this.quill.setSelection(range.index + 8);
-        } else {
-          this.quill.insertText(range.index - 2, ' ' + equalsUnicode + ' ');
-          this.quill.setSelection(range.index + 1);
-        }
-      }
-  },
-  less:{
-    key: 't',
-    empty:false,
-    prefix: /;l$/,
-    handler: function(range,context){
-        this.quill.deleteText(range.index - 2, 2);
-        if(context.offset == 2){
-          this.quill.insertText(range.index - 2, lessThanUnicode + "  " + hintUnicode);
-          this.quill.setSelection(range.index + 8);
-        } else {
-          this.quill.insertText(range.index - 2, ' ' + lessThanUnicode + ' ');
-          this.quill.setSelection(range.index + 1);
-        }
-      }
-  },
-  lessE:{
-    key: 'e',
-    empty:false,
-    prefix: /;l$/,
-    handler: function(range,context){
-        this.quill.deleteText(range.index - 2, 2);
-        if(context.offset == 2){
-          this.quill.insertText(range.index - 2, lessThanOrEqUnicode + "  " + hintUnicode);
-          this.quill.setSelection(range.index + 8);
-        } else {
-          this.quill.insertText(range.index - 2, ' ' + lessThanOrEqUnicode + ' ');
-          this.quill.setSelection(range.index + 1);
-        }
-      }
-  },
-  greater:{key: 't',
-  empty:false,
-  prefix: /;g$/,
-  handler: function(range,context){
-      this.quill.deleteText(range.index - 2, 2);
-      if(context.offset == 2){
-        this.quill.insertText(range.index - 2, greaterThanUnicode + "  " + hintUnicode);
-        this.quill.setSelection(range.index + 8);
-      } else {
-        this.quill.insertText(range.index - 2, ' ' + greaterThanUnicode + ' ');
-        this.quill.setSelection(range.index + 1);
-      }
-    }},
-  greaterE:{
-    key: 'e',
-    empty:false,
-    prefix: /;g$/,
-    handler: function(range,context){
-        this.quill.deleteText(range.index - 2, 2);
-        if(context.offset == 2){
-          this.quill.insertText(range.index - 2, greaterThanorEqUnicode + "  " + hintUnicode);
-          this.quill.setSelection(range.index + 8);
-        } else {
-          this.quill.insertText(range.index - 2, ' ' + greaterThanorEqUnicode + ' ');
-          this.quill.setSelection(range.index + 1);
-        }
-      }
-  },
-}
+  }}
+//   parenthesis:{
+//     key: '9',
+//     shiftKey: true,
+//     handler: function(range,context){
+//       this.quill.insertText(range.index, ' (  ) ');
+//       this.quill.setSelection(range.index + 3);
+//     }
+//   },
+//   curlyBrackets:{
+//     key: 219,
+//     shiftKey: true,
+//     handler: function(range,context){
+//       this.quill.insertText(range.index, ' {  } ');
+//       this.quill.setSelection(range.index + 3);
+//     }
+//   },
+//   brackets:{
+//     key: 219,
+//     handler: function(range,context){
+//       this.quill.insertText(range.index, ' [  ] ');
+//       this.quill.setSelection(range.index + 3);
+//     }
+//   }
+// }
 
-var Quill = require('quill');
 var editor = new Quill('#editor', {
 	modules: {
-		toolbar: {container: '#toolbar'},
+		// toolbar: {container: '#toolbar'},
     keyboard: {bindings: bindings}
-	},
-	theme: 'snow'
+  },
+  placeholder: '     Enter your proof...',
+	theme: 'bubble'
 });
-// Save and Load files
 
+// this.bindKey(this.editor, ' ≺ ', ';i', 't', false, true);                                                      // it-relation
+    // this.bindHint(this.editor, this.impliesUnicode + this.shortHintUnicode, ';i', 'm');             // implies
+    // this.bindHint(this.editor, this.impliesUnicode + this.shortHintUnicode, '>', 190, true);        // implies
+    // this.bindHint(this.editor, this.followsFromUnicode + this.shortHintUnicode, ';f', 'f');         // follows from
+    // this.bindHint(this.editor, this.followsFromUnicode + this.shortHintUnicode, '<', 188, true);    // follows from
+    // this.bindHint(this.editor, this.equalsUnicode + this.hintUnicode, '', 187);                     // equals
+    // this.bindHint(this.editor, this.lessThanUnicode + this.hintUnicode, ';l', 't');                 // less than
+    // this.bindHint(this.editor, this.lessThanOrEqUnicode + this.hintUnicode, ';l', 'e');             // less than or equal to
+    // this.bindHint(this.editor, this.greaterThanUnicode + this.hintUnicode, ';g', 't');              // greater than
+    // this.bindHint(this.editor, this.greaterThanorEqUnicode + this.hintUnicode, ';g', 'e');          // greater than or equal to
+
+    // this.bindPair(this.editor, '()', '', '9', true, false);                                          // parentheses
+    // this.bindPair(this.editor, '{}', '', 219, true, false);                                          // curly braces
+    // this.bindPair(this.editor, '[]', '', 219, false, false);                                         // square brackets
+;
+    this.bindKey(this.editor, ' ≺ ', ';i', 't', false, true);                                                      // it-relation
+    this.bindKey(this.editor, ' ⪯ ', ';ra', 't', false, true);                                                     // rat-relation
+    this.bindKey(this.editor, ' ≻ ', ';ii', 't', false, true);                                                     // inverse it-relation
+    this.bindKey(this.editor, ' ⪰ ', ';ira', 't', false, true);                                                    // inverse rat-relation
+    this.bindKey(this.editor, ' ' + impliesUnicode + ' ', ';i', 'm', false, true);                                 // implies
+    this.bindKey(this.editor, ' ' + impliesUnicode + ' ', '>', 190, true, true);                            // implies
+    this.bindKey(this.editor, ' ' + followsFromUnicode + ' ', ';f', 'f', false, true);                             // follows from
+    this.bindKey(this.editor, ' ' + followsFromUnicode + ' ', '<', 188, true, true);                        // follows from
+    this.bindKey(this.editor, ' ' + lessThanUnicode + ' ', ';l', 't', false, true);                                // less than
+    this.bindKey(this.editor, ' ' + greaterThanUnicode + ' ', ';g', 't', false, true);                             // greater than
+    this.bindKey(this.editor, ' ' + lessThanOrEqUnicode + ' ', ';l', 'e', false, true);                            // less than or equal
+    this.bindKey(this.editor, ' ' + greaterThanorEqUnicode + ' ', ';g', 'e',false, true);                         // greater than or equal
+    this.bindKey(this.editor, ' ' + equivalesUnicode + ' ', '=', 187, false, true);                                // equivales
+    this.bindKey(this.editor, ' ' + conjuctionUnicode + ' ', ';a', 'n', false, true);                              // conjunction
+    this.bindKey(this.editor, ' ' + conjuctionUnicode + ' ', '&', '7', true, true);                         // conjunction
+    this.bindKey(this.editor, ' ' + disjunctionUnicode + ' ', ';o', 'r', false, true);                             // disjunction
+    this.bindKey(this.editor, ' ' + disjunctionUnicode + ' ', '\\|', 220, true, true);                      // disjunction
+    this.bindKey(this.editor, '¬', '!', '1', true, true);                                                  // negation
+    this.bindKey(this.editor, ' ' + notEquivalesUnicode + ' ', '!', 187, false, true);                //not equivales
+    this.bindKey(this.editor, ' ' + textSubUnicode + ' ', ':', 187, false, true);                                  // replace by/gets
+    this.bindKey(this.editor, ' → ', '-', 190, true, true);                                                 // right arrow
+    this.bindKey(this.editor, ' ' + leftBracketUnicode + ' ', ';l', 'b', false, true);                             // left hint bracket
+    this.bindKey(this.editor, ' ' + rightBracketUnicode, ';r', 'b', false, true);                                  // right hint bracket
+    this.bindKey(this.editor, ' ' + elementOfUnicode + ' ', ';e', 'l', false, true);                               // element of
+    this.bindKey(this.editor, ' ' + unionUnicode + ' ', ';u', 'n', false, true);                                   // union
+    this.bindKey(this.editor, ' ' + unionUnicode + ' ', '\\|', 220, false, true);                                  // union
+    this.bindKey(this.editor, ' ' + intersectionUnicode + ' ', ';i', 'n', false, true);                            // intersection
+    this.bindKey(this.editor, ' ' + intersectionUnicode + ' ', '&', '7', false, true);                             // intersection
+    this.bindKey(this.editor, ' ÷ ', '/', 191, false, true);                                                       // division symbol
+    this.bindKey(this.editor, ' ⋅ ', '\\*', '8', true, true);                                               // multiplication symbol
+    this.bindKey(this.editor, ' ' + genQuantifierUnicode + ' ', ';s', 't', false, true);                           // star symbol
+    this.bindKey(this.editor, ' ' + universalQuantifierUnicode, ';f', 'a', false, true);                           // forAll symbol
+    this.bindKey(this.editor, ' ' + existentialQuanitiferUnicode, ';e', 'x', false, true);                         // exists symbol
+
+    // this.bindKey(this.editor, 'Name:\t\t\t\t\nPin:\t\t\t\t\t\nClass:\t\t\t\t\nAssignment:\t\n\nProve ', ';hea', 'd');
+    // this.bindHint(this.editor, 'Prove ', ';p', 'r');
+    // this.bindKey(this.editor, 'by showing equivalence to previous theorem', ';', '1');
+    // this.bindKey(this.editor, 'by showing the LHS is equivalent to the RHS', ';', '2');
+    // this.bindKey(this.editor, 'by showing the RHS is equivalent to the LHS', ';', '3');
+    // this.bindKey(this.editor, 'by showing the LHS implies the RHS', ';', '4');
+    // this.bindKey(this.editor, 'by showing the LHS follows from the RHS', ';', '5');
+    // this.bindKey(this.editor, 'by assuming the conjuncts of the antecedent', ';', '6');
+    // this.bindKey(this.editor, '\nwhich is ', ';', 'w');
+    // this.bindKey(this.editor, 'textual substitution ', ';t', 's');
+
+    // this.bindQuant(this.editor, '(' + this.genQuantifierUnicode + ' |  : )', ';g', 'q');
+    // this.bindQuant(this.editor, '(' + this.universalQuantifierUnicode + ' |  : )', ';u', 'q');
+    // this.bindQuant(this.editor, '(' + this.existentialQuanitiferUnicode + ' |  : )', ';e', 'q');
+// Save and Load files
 var loadedfs;
 
 function format(){
@@ -289,4 +225,54 @@ function readFromFile(editor, filename) {
         }
         editor.setText(data);
     });
+}
+
+
+function bindKey(quill, text, prefix, key, shift, replace, hint, pair, quant) {
+  // console.log('keySeq type = ' + typeof keySeq);
+  // const key: string = (typeof keySeq === 'number' ? keySeq : keySeq.substr(keySeq.length - 1, 1));
+  // const prefix: string = (typeof keySeq === 'number' ? '' : (keySeq.length > 1 ? keySeq.substr(0, keySeq.length - 1) : ''));
+  let bindObj;
+  if (shift) {
+    bindObj = {key: key, shiftKey: true};
+  } else {
+    bindObj = {key: key};
+  }
+  const anchor = (hint ? '\\s{5}' : '\\S*');
+  const bindOptions = {
+    collapsed: true,
+    prefix: new RegExp(anchor + prefix + '$'),
+    // offset: -10
+  };
+  if (hint) {
+    // bindOptions.offset = 5 + prefix.length;
+  }
+  this.editor.keyboard.addBinding(bindObj, bindOptions,
+  (range, context) => {
+    this.editor.format('bold', false);
+    this.editor.format('italic', false);
+    let preLength = prefix.length;
+    console.log('prelength: ' + preLength);
+    if (prefix.startsWith('\\')) {
+        preLength--;
+    }
+    const off = (hint ? this.spacing + preLength : preLength);
+    if (replace) {
+      this.editor.deleteText(range.index - off, off);
+    }
+    this.editor.insertText(range.index - (replace ? off : 0), text);
+    this.editor.setSelection(range.index + (hint ? + text.length - 7 - preLength : preLength + text.length - (pair ? 1 : quant ? 11 : 2)));
+  });
+}
+
+function bindHint(quill, text, prefix, key, shift, replace) {
+  this.bindKey(this.editor, text, prefix, key, shift, replace, true);
+}
+
+function bindPair(quill, text, prefix, key, shift, replace) {
+  this.bindKey(this.editor, text, prefix, key, shift, replace, false, true);
+}
+
+function bindQuant(quill, text, prefix, key, shift, replace) {
+  this.bindKey(this.editor, text, prefix, key, shift, replace, false, false, true);
 }
